becd49f184888815ec15415c18d24f4e5ef8462d
dotoryeee <bayer@kakao.com>
2021-05-23 00:03:58 +0900

    add: searchButton EventListner

---
+++ loadJSON.js
@@ -0,0 +1,15 @@
+const searchButton = document.getElementsByClassName('btn')
+
+function handleSubmit(event){
+  fetch('http://api.nugaspam.com/check/bit.ly/33N8hyQ')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(spamdata) {
+    console.log(JSON.stringify(spamdata));
+  });
+}
+
+function init(){
+  searchButton.addEventListener("submit", handleSubmit)
+}
